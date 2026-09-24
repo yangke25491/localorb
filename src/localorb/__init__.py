4e9e299457b1128c721afcb3b3458710b202bbdf
ke yang@SYSU <82886568+yangke25491@users.noreply.github.com>
2026-09-24 12:23:26 +0800

    Add package initializer

---
+++ src/localorb/__init__.py
@@ -0,0 +1,6 @@
+"""localorb: structure-aware local orbital frames."""
+
+from .frames import LocalFrame, build_local_frame, find_center_sites
+
+__all__ = ["LocalFrame", "build_local_frame", "find_center_sites"]
+__version__ = "0.1.0"
